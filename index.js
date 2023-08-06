cb13adb3547807b39a28d7c8c9a7ee8f7122f225
mirocow <mr.mirocow@gmail.com>
2023-08-06 03:58:01 +0300

    Added Xpath or JSONPath search by post body

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { JSONPath } = require('jsonpath-plus');
+
 module.exports.templateTags = [
   {
     name: 'otherrequest',
@@ -17,6 +19,11 @@ module.exports.templateTags = [
             displayName: 'QueryParam',
             description: 'value of request query param',
             value: 'query'
+          },
+          {
+            displayName: 'Post body',
+            description: 'value of request body',
+            value: 'body'
           }
         ]
       },
@@ -34,7 +41,7 @@ module.exports.templateTags = [
     async run(context, field, id, filter) {
       filter = filter || '';
 
-      if (!['formfield', 'query'].includes(field)) {
+      if (!['formfield', 'query', 'body'].includes(field)) {
         throw new Error(`Invalid response field '${field}'`);
       }
 
@@ -57,39 +64,171 @@ module.exports.templateTags = [
         throw new Error('No successful responses for request');
       }
 
-      const sanitizedFilter = filter.trim();
-
-      let attributes = [];
       if (field === 'formfield') {
         if (!request.body.params) {
           throw new Error('No formfields for request');
         }
-        attributes = request.body.params;
+        return searchByParams(request.body.params. filter);
+      } else if (field === 'body') {
+        if (!request.body.text) {
+          throw new Error('No body for request');
+        }
+        return searchByXpath(request.body.text, filter);
       } else if (field === 'query') {
         if (!request.parameters) {
           throw new Error('No query for request');
         }
-        attributes = request.parameters;
+        return searchByParams(request.parameters, filter);
       } else {
         throw new Error(`Unknown field ${field}`);
       }
-
-      const choices = attributes.filter(attr => !attr.disabled)
-          .map(attr => attr.name)
-          .filter((value, index, self) => self.indexOf(value) === index)
-          .join(',\n\t');
-
-      if (!filter) {
-        throw new Error("No attribute name given. Choices:\n\t" + choices);
-      }
-
-      for (const attr of attributes) {
-        if (!attr.disabled && attr.name === sanitizedFilter) {
-          return context.util.render(attr.value);
-        }
-      }
-
-      throw new Error(`Attribute name '${sanitizedFilter}' not found. Choices:\n\t${choices}`);
     }
   }
 ];
+
+/**
+ * 
+ * @param {*} attributes 
+ * @param {*} filter 
+ * @returns 
+ */
+function searchByParams(attributes, filter)
+{
+    const sanitizedFilter = filter.trim();
+
+    const choices = attributes.filter(attr => !attr.disabled)
+      .map(attr => attr.name)
+      .filter((value, index, self) => self.indexOf(value) === index)
+      .join(',\n\t');
+
+    if (!filter) {
+      throw new Error("No attribute name given. Choices:\n\t" + choices);
+    }
+
+    for (const attr of attributes) {
+        if (!attr.disabled && attr.name === sanitizedFilter) {
+          return context.util.render(attr.value);
+        }
+    }
+
+    throw new Error(`Attribute name '${sanitizedFilter}' not found. Choices:\n\t${choices}`);
+}
+
+/**
+ * 
+ * @param {*} body 
+ * @param {*} filter 
+ * @returns 
+ */
+function searchByXpath(body, filter)
+{
+    if (!filter) {
+      filter = '$';
+    }
+
+    const sanitizedFilter = filter.trim();
+
+    if (sanitizedFilter.indexOf('$') === 0) {
+      return matchJSONPath(body, sanitizedFilter);
+    } else {
+      return matchXPath(body, sanitizedFilter);
+    }
+}
+
+function matchJSONPath(bodyStr, query) {
+  let body;
+  let results;
+
+  try {
+    body = JSON.parse(bodyStr);
+  } catch (err) {
+    throw new Error(`Invalid JSON: ${err.message}`);
+  }
+
+  try {
+    results = JSONPath({json: body, path: query});
+  } catch (err) {
+    throw new Error(`Invalid JSONPath query: ${query}`);
+  }
+
+  if (results.length === 0) {
+    throw new Error(`Returned no results: ${query}`);
+  }
+
+  if (results.length > 1 ) {
+    return JSON.stringify(results);
+  }
+
+  if (typeof results[0] !== 'string') {
+    return JSON.stringify(results[0]);
+  } else {
+    return results[0];
+  }
+}
+
+function matchXPath(bodyStr, query) {
+  const results = queryXPath(bodyStr, query);
+
+  if (results.length === 0) {
+    throw new Error(`Returned no results: ${query}`);
+  } else if (results.length > 1) {
+    throw new Error(`Returned more than one result: ${query}`);
+  }
+
+  return results[0].inner;
+}
+
+/**
+ * Query an XML blob with XPath
+ */
+const queryXPath = (xml, query) => {
+  const DOMParser = require('xmldom').DOMParser
+  const dom = new DOMParser().parseFromString(xml);
+  let selectedValues = [];
+  if (query === undefined) {
+    throw new Error('Must pass an XPath query.');
+  }
+  try {
+    selectedValues = require('xpath').select(query, dom);
+  } catch (err) {
+    throw new Error(`Invalid XPath query: ${query}`);
+  }
+  const output = [];
+  // Functions return plain strings
+  if (typeof selectedValues === 'string') {
+    output.push({
+      outer: selectedValues,
+      inner: selectedValues,
+    });
+  } else {
+    for (const selectedValue of selectedValues || []) {
+      switch (selectedValue.constructor.name) {
+        case 'Attr':
+          output.push({
+            outer: selectedValue.toString().trim(),
+            inner: selectedValue.nodeValue,
+          });
+          break;
+
+        case 'Element':
+          output.push({
+            outer: selectedValue.toString().trim(),
+            inner: selectedValue.childNodes.toString(),
+          });
+          break;
+
+        case 'Text':
+          output.push({
+            outer: selectedValue.toString().trim(),
+            inner: selectedValue.toString().trim(),
+          });
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+  return output;
+};
+
